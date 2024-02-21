b816009f4121a135f8082a65d49f79faeceefe34
Shashank <mshashank02@gmail.com>
2024-02-21 12:34:49 +0530

    added rigids

--- scripts/synchronise_node.py
+++ scripts/synchronise_node.py
@@ -2,8 +2,8 @@
 
 import rclpy
 from rclpy.node import Node
-from phasespace_msgs.msg import Marker
-from phasespace_msgs.msg import Markers
+from phasespace_msgs.msg import Rigid
+from phasespace_msgs.msg import Rigids
 from rclpy.qos import qos_profile_sensor_data
 from message_filters import Subscriber, ApproximateTimeSynchronizer
 from ackermann_msgs.msg import AckermannDriveStamped
@@ -20,33 +20,32 @@ class SynchronizeAndPublish(Node):
         self.ackermann_publisher = self.create_publisher(AckermannDriveStamped, '/synchronized_ackermann', 10)
 
         # Publisher for Markers
-        self.markers_publisher = self.create_publisher(Markers, '/synchronized_markers', 10)
+        self.rigids_publisher = self.create_publisher(Rigids, '/synchronized_rigids', 10)
 
         # Subscribers for Ackerman Command and Phasespace Markers
         ackermann_sub = Subscriber(self, AckermannDriveStamped, '/ackermann_cmd', qos_profile=qos_profile_sensor_data)
-        markers_sub = Subscriber(self, Markers, '/phasespace/markers', qos_profile=qos_profile_sensor_data)
+        rigids_sub = Subscriber(self, Rigids, '/phasespace/rigids', qos_profile=qos_profile_sensor_data)
 
         # Synchronize messages based on ROS time
-        self.sync = ApproximateTimeSynchronizer([ackermann_sub, markers_sub], queue_size=10, slop=0.1)
+        self.sync = ApproximateTimeSynchronizer([ackermann_sub, rigids_sub], queue_size=10, slop=0.1)
         self.sync.registerCallback(self.callback)
 
-    def callback(self, ackermann_msg, markers_msg):
+    def callback(self, ackermann_msg, rigids_msg):
         # Process Ackermann message
         steering_angle = ackermann_msg.drive.steering_angle
         self.get_logger().info(f"Received Ackermann message - Steering Angle: {steering_angle}")
         self.ackermann_publisher.publish(ackermann_msg)
 
-        markers_array = markers_msg.markers
-        for marker in markers_array:
-            print(marker.id)
-            print(marker.x)
-            print(marker.y)
-
-        # Process Markers message
-        
+        #Process Rigids message
+        rigid_array = rigids_msg.rigids
+        for rigid in rigid_array:
+            print(rigid.id)
+            print(rigid.x)
+            print(rigid.y)
+            print(rigid.qz)
 
         # Publish the synchronized Markers message
-        self.markers_publisher.publish(markers_msg)
+        self.rigids_publisher.publish(rigids_msg)
 
 def main(args=None):
     rclpy.init(args=args)

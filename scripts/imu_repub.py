09cad7cd5410c122da40bda691d4a8cfb2caaee7
Shashank <mshashank02@gmail.com>
2024-03-22 21:48:10 +0530

    added imu_repub

--- scripts/imu_repub.py
+++ scripts/imu_repub.py
@@ -24,25 +24,25 @@ class ImuToOdometry(Node):
         #Publisher for Odometry 
         self.imu_odom_publisher = self.create_publisher(Odometry, '/odom2',10)
 
-        def imu_callback(self,msg):
+    def imu_callback(self,msg):
 
-                odom_msg = Odometry()
+            odom_msg = Odometry()
         
 
-                odom_msg.pose.orientation.x = msg.imu.orientation.x
-                odom_msg.pose.orientation.y = msg.imu.orientation.y
-                odom_msg.pose.orientation.z = msg.imu.orientation.z
-                odom_msg.pose.orientation.w = msg.imu.orientation.w
+            odom_msg.pose.orientation.x = msg.imu.orientation.x
+            odom_msg.pose.orientation.y = msg.imu.orientation.y
+            odom_msg.pose.orientation.z = msg.imu.orientation.z
+            odom_msg.pose.orientation.w = msg.imu.orientation.w
 
-                odom_msg.twist.angular.x = msg.imu.angular_velocity.x
-                odom_msg.twist.angular.y = msg.imu.angular_velocity.y
-                odom_msg.twist.angular.z = msg.imu.angular_velocity.z
+            odom_msg.twist.angular.x = msg.imu.angular_velocity.x
+            odom_msg.twist.angular.y = msg.imu.angular_velocity.y
+            odom_msg.twist.angular.z = msg.imu.angular_velocity.z
 
-                odom_msg.header.stamp = self.get_clock().now().to_msg()
-                odom_msg.header.frame_id = 'odom'
-                odom_msg.child_frame_id = 'base_link'
+            odom_msg.header.stamp = self.get_clock().now().to_msg()
+            odom_msg.header.frame_id = 'odom'
+            odom_msg.child_frame_id = 'base_link'
 
-                self.imu_odom_publisher.publish(odom_msg)
+            self.imu_odom_publisher.publish(odom_msg)
 
 def main(args=None):
      
